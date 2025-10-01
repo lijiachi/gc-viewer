c2e0b7cf6864cd032526f72cc26b87e4b5f4cd2e
Chandler <18461031174@163.com>
2025-10-01 17:06:10 +0800

    增加类型转换的实例代码Day251001.java

---
+++ src/main/java/org/practice/Day251001.java
@@ -0,0 +1,26 @@
+package org.practice;
+class Animal{
+    private String name;
+    Animal(String name){
+        this.name = name;
+    }
+}
+class Dog extends Animal{
+    public Dog(String name){
+        super(name);
+    }
+}
+public class Day251001 {
+    public static void main(String[] args){
+        //常见运算符——略
+        //复杂类型隐式转换dog -> animal
+        Animal animal = new Dog("动物");
+        //复杂类型显式转换animal ->dog
+        Dog dog = (Dog)animal;
+        //(byte、short和char),进行+等运算时先隐式转换到int
+        byte a = 1;
+        byte b = 2;
+        byte c = a + b;//a+b时进行了隐式转换byte->int编译错误由此可以倒推一下编译逻辑
+
+    }
+}
